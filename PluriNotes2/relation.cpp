5cd22bffab387cbd2025e108a4d1bfa29708d47c
guillaume-le-fur <guillaume.lefur29000@gmail.com>
2017-06-15 09:56:43 +0200

    commentaires dans relation.h

--- PluriNotes2/relation.cpp
+++ PluriNotes2/relation.cpp
@@ -5,7 +5,7 @@ unsigned int RelationManager::nbMax = 0;
 RelationManager* RelationManager::uniqueInstance = 0;
 Relation** RelationManager::relations = 0;
 Reference* Reference::uniqueRef = 0;
-QString RelationManager::directory = "/home/hugo/";
+QString RelationManager::directory = "/home/guilllaume/Documents/UTC/GI02/LO21/LO21_plurinotes/LO21_plurinotes/PluriNotes2/";
 
 ostream& operator << (ostream& f, Relation& R){
     f<<"----------Relation - "<<R.getTitle()<<"---------"<<endl;
--- PluriNotes2/relation.h
+++ PluriNotes2/relation.h
@@ -15,34 +15,36 @@ using namespace std;
  */
 class Couple {
 private :
-    QString label;
-    Note* x;
-    Note* y;
+    QString label;//!label du couple
+    Note* x;//!Note du couple
+    Note* y;//!Not du couple
 public :
-    Couple(const QString& l, Note* x , Note* y): label(l), x(x), y(y) {}
+    Couple(const QString& l, Note* x , Note* y): label(l), x(x), y(y) {}//!Constructeur de Couple
+    Couple(): label(""), x(new Note), y(new Note){}//!redéfinition du constructeur
     //getters
-    Couple(): label(""), x(new Note), y(new Note){}
-    const QString& getLabel() const{return label;}
-    const Note* getX() const{return x;}
-    const Note* getY() const{return y;}
+    const QString& getLabel() const{return label;}//!getter de label
+    const Note* getX() const{return x;}//!getter de x
+    const Note* getY() const{return y;}//!getter de y
 
-    QString& getLabel(){return label;}
-    Note* getX(){return x;}
-    Note* getY(){return y;}
+    QString& getLabel(){return label;}//!getter de label
+    Note* getX(){return x;}//!getter de x
+    Note* getY(){return y;}//!getter de y
     //setters
-    void setX(Note * Xs){x = Xs;}
-    void setY(Note * Ys){y = Ys;}
-    /*!
-     * \brief fromJson ajoute les informations contenues dans le
-     * fichier json à l'intérieur du Couple
-     * \param j l'objet de type json
-     */
-    json toJson();
+    void setX(Note * Xs){x = Xs;}//!setter de x
+    void setY(Note * Ys){y = Ys;}//!setter de y
+
     /*!
      * \brief toJson insère le contenu d'un objet Couple dans un objet de
      * type json
      * \return Le fichier json contenant les informations du Couple
     */
+    json toJson();
+
+    /*!
+     * \brief fromJson ajoute les informations contenues dans le
+     * fichier json à l'intérieur du Couple
+     * \param j l'objet de type json
+     */
     static Couple& fromJson(json j);
 };
 
@@ -51,43 +53,36 @@ public :
  */
 class Relation {
 private :
-    Couple ** couples;
-    unsigned int nb;
-    unsigned int nbMax;
-    QString title;
-    QString description;
-    RelationOrientation orientation;
+    Couple ** couples;//!tableau de pointeur de couples
+    unsigned int nb;//!nombre de couples dnas le tableau
+    unsigned int nbMax;//!nombre maximal de couples
+    QString title;//!titre de la relation
+    QString description;//!description de la relation
+    RelationOrientation orientation;//!orientation de la relation
 public :
     Relation(Couple ** c = new Couple*[0], unsigned int n = 0, unsigned int nM = 0, const QString& t = "", const QString& d = "", const RelationOrientation& o = oriented)
         : couples(new Couple*[nM]), nb(n), nbMax(nM), title(t), description(d), orientation(o){
         for (unsigned int i = 0 ; i< n ; i++){
             couples[i] = c[i];
         }
-    }
+    }//!constructeur de realtion
     Relation(const RelationOrientation& o)
         : couples(new Couple*[0]), nb(0), nbMax(0), title(""), description(""), orientation(o){
-//        for (unsigned int i = 0 ; i< n ; i++){
-//            couples[i] = c[i];
-    }
+    }//!redéfinition du consturcteur de Relation
 
-    virtual ~Relation(){
-//        for(unsigned int i=0 ; i<nb ; i++){
-//            delete couples[i];
-//        }
-//        delete[] couples;
-    }
+    virtual ~Relation(){}//!destructeur de Relation
 
     //getters
-    const unsigned int& getNb() const{return nb;}
-    const unsigned int& getNbMax() const{return nbMax;}
-    const QString& getTitle() const{return title;}
-    const QString& getDescription() const{return description;}
-    const RelationOrientation& getOrientation() const{return orientation;}
-    unsigned int& getNb(){return nb;}
-    unsigned int& getNbMax(){return nbMax;}
-    QString& getTitle(){return title;}
-    QString& getDescription(){return description;}
-    RelationOrientation& getOrientation(){return orientation;}
+    const unsigned int& getNb() const{return nb;}//!getter de nb
+    const unsigned int& getNbMax() const{return nbMax;}//!getter de nbMax
+    const QString& getTitle() const{return title;}//!getter de title
+    const QString& getDescription() const{return description;}//!getter de description
+    const RelationOrientation& getOrientation() const{return orientation;}//!getter de orientation
+    unsigned int& getNb(){return nb;}//!getter de  nb
+    unsigned int& getNbMax(){return nbMax;}//!getter de nbMax
+    QString& getTitle(){return title;}//!getter de title
+    QString& getDescription(){return description;}//!getter de description
+    RelationOrientation& getOrientation(){return orientation;}//!getter de orientation
 
     QString getOrientationQS(){
         switch(orientation){
@@ -97,31 +92,42 @@ public :
             return "non orientée";
         }
         //return "orientée";
-    }
+    }//!getter de orientation sous forme de QString
 
     //setters
-    void setNb(unsigned int n){nb = n;}
-    void setNbMax(unsigned int nM){nbMax = nM;}
-    void setTitle(const QString& t){title = t;}
-    void setDescription(const QString& d){description = d;}
-    void setOrientation(RelationOrientation& ori){orientation = ori;}
-    void setCouple(Couple ** C){couples = C;} // TO DELETE ? (NOT USED)
+    void setNb(unsigned int n){nb = n;}//!setter de nb
+    void setNbMax(unsigned int nM){nbMax = nM;}//!setter de nbMax
+    void setTitle(const QString& t){title = t;}//!setter de title
+    void setDescription(const QString& d){description = d;}//!setter de description
+    void setOrientation(RelationOrientation& ori){orientation = ori;}//!setter de orientation
+    //void setCouple(Couple ** C){couples = C;} // TO DELETE ? (NOT USED)
+    //autres méthodes :
+    /*!
+     * \brief addCouple ajout eun couple à la relation
+     * \param c le couple à ajouter
+     */
     void addCouple(Couple * c);
-    void deleteCouple(Couple * c); // TODO
+
+    /*!
+     * \brief deleteCouple Supprime le couple en paramètre de la relation
+     * \param c le couple à supprimer
+     */
+    void deleteCouple(Couple * c);
 
     /*!
      * \brief fromJson ajoute les informations contenues dans le
      * fichier json à l'intérieur du Relation
      * \param j l'objet de type json
      */
-    json toJson();
+    static Relation& fromJson(json j);
+
 
     /*!
      * \brief toJson insère le contenu d'un objet Relation dans un objet de
      * type json
      * \return Le fichier json contenant les informations de la Relation
     */
-    static Relation& fromJson(json j);
+    json toJson();
 
     class iterator{
         friend class Relation;
@@ -134,7 +140,6 @@ public :
         bool isDone(){return nbRemain==0;}
         void isNext(){
             if(isDone())
-                // TODO change comments
                 throw NotesException("Error, no more versions of notes");
             currentR++; nbRemain--;
         }
@@ -161,15 +166,12 @@ public :
  * \brief La classe RelationManager permet de gérer l'ensemble des relations, c'est un singleton.
  */
 class RelationManager {
- //Il faut déclarer pas mal de trucs static ici pour pouvoir utiliser les relations dans les autres classes
- //A mon avis relationManager devra être un singleton, en plus on aurait aucun intérêt à avoir plusieurs
- //RelationManagers.
 private :
-    static RelationManager * uniqueInstance;
-    static Relation ** relations;
-    static unsigned int nb;
-    static unsigned int nbMax;
-    static QString directory;
+    static RelationManager * uniqueInstance;//!Instance unique du singleton
+    static Relation ** relations;//!tableau de pointeurs de relations
+    static unsigned int nb;//!nombre de relations dans le tableau
+    static unsigned int nbMax;//!nombre maximal de relations
+    static QString directory;//!chemin vers l'emplacement de sauvegarde des relations
     RelationManager(Relation ** r = new Relation*[0], unsigned int n = 0, unsigned int nM = 0){
         nb = n;
         nbMax = nM;
@@ -177,26 +179,54 @@ private :
         for (unsigned int i = 0 ; i< n ; i++){
             relations[i] = r[i];
         }
-    }
+    }//!constructeur de RelationManager
+    /*!
+     * \brief ~RelationManager destructeur de Relation Manager
+     */
     virtual ~RelationManager();
+    /*!
+     * \brief operator = redéfinition de l'opérateur d'affectation
+     * \param RM le RM à affecter
+     */
     RelationManager& operator= (const RelationManager& RM);
+    /*!
+     * \brief RelationManager constructeur par recopie de RM
+     * \param RM le RM à recopier
+     */
     RelationManager(const RelationManager& RM);
 
 public :
+    /*!
+     * \brief getRelationManager alloue de la mémoire pour l'instance unique et la retourne
+     * \return l'unique instance de RM
+     */
     static RelationManager& getRelationManager(Relation ** r, unsigned int n, unsigned int nM);
+    /*!
+     * \brief getRelationManager redéfinition de la méthode pour récupérer un RM
+     */
     static RelationManager& getRelationManager();
+    /*!
+     * \brief freeRelationManager libère l'espace mémoire utilisé par le RM
+     */
     static void freeRelationManager();
+
     //getters
+    static unsigned int& getNb(){return nb;}//!getter de nb
+    static unsigned int& getNbMax(){return nbMax;}//!getter de nbMax
 
-    static unsigned int& getNb(){return nb;}
-    static unsigned int& getNbMax(){return nbMax;}
     //setters
-    static void setNb(unsigned int n){nb = n;}
-    static void setNbMax(unsigned int nM){nbMax = nM;}
+    static void setNb(unsigned int n){nb = n;}//!setter de nb
+    static void setNbMax(unsigned int nM){nbMax = nM;}//!setter de nbMax
 
+    /*!
+     * \brief addRelation ajoute une relation au tableau de relations
+     * \param R la relation à ajouter.
+     */
     static void addRelation(Relation* R);
+
     //faire un iterator
-    static Relation* getNthElement(unsigned int n){return relations[n];}
+    //static Relation* getNthElement(unsigned int n){return relations[n];}
+
     /*!
      * \brief getDescendants renvoie l'ensemble des descendants d'une note
      * \param N La note en question
@@ -204,23 +234,37 @@ public :
      * \return un map de la forme <Note* ,  ordre auquel la Note* à été trouvée>
      */
     static map<Note *, int> getDescendants(Note* N, int order = 2);
+
+    /*!
+     * \brief getAscendants renvoie l'ensemble des ascendants d'une note
+     * \param N La note en question
+     * \param order l'ordre jusqu'auquel on veut chercher des ascendants
+     * \return un map de la forme <Note* ,  ordre auquel la Note* à été trouvée>
+     */
     static map<Note *, int> getAscendants(Note* N, int order = 2);
 
     /*!
-     * \brief fromJson ajoute les informations contenues dans le
-     * fichier json à l'intérieur du RelationManager
-     * \param j l'objet de type json
-     */
+     * \brief toJson insère le contenu d'un objet RelationManager dans un objet de
+     * type json
+     * \return Le fichier json contenant les informations du RelationManager
+    */
     json toJson();
 
+    /*!
+     * \brief save Sauvegarde les informations contenues dans le RelationManager dans la mémoire à l'endroit ou est défini directory
+     */
     void save();
+    /*!
+     * \brief load récupère les informations contenues dans le fichier directory/relations.json
+     */
     void load();
+
 
     class iterator{
         friend class RelationManager;
     private:
         unsigned int nbRemain;
-        Relation** currentRM; // TODO CHANGE ??? iterate through Relation not RM !?
+        Relation** currentRM;
         iterator(Relation** R, unsigned int nb):nbRemain(nb), currentRM(R){}
     public:
         unsigned int getNbRemain()const{return nbRemain;}
@@ -228,7 +272,6 @@ public :
         void isNext(){
 
             if(isDone())
-                // TODO change comments
                 throw NotesException("Error, no more versions of notes RM");
             currentRM++;
             nbRemain--;
@@ -249,25 +292,58 @@ public :
 
 class Reference : public Relation {
 private :
-    static Reference * uniqueRef;
+    static Reference * uniqueRef;//!instance unique de référence
     Reference(Couple ** c = new Couple*[0], unsigned int n = 0, unsigned int nM = 0, const QString& t = "References", const QString& d = "Cette realtion contient les références", const RelationOrientation& o = oriented)
-        : Relation(c, n, nM, t, d, o){}
-    ~Reference(){}
+        : Relation(c, n, nM, t, d, o){}//!construteur de Reference
+    ~Reference(){}//!destructeur de Reference
+    /*!
+     * \brief Reference Constructeur par recopie de Reference
+     * \param r la reference à copier
+     */
     Reference(const Reference& r);
+    /*!
+     * \brief operator = Redéfinition de l'opérateur d'affectation
+     * \param r la Referecne à affecter
+     */
     Reference& operator=(const Reference& r);
 
 public :
+    /*!
+     * \brief getRef alloue de la mémoire à l'unique instance et la retourne
+     * \return l'unique instance
+     */
     static Reference& getRef();
+    /*!
+     * \brief freeRef libère l'espace mémoire occupé par l'unique instance
+     */
     static void freeRef();
+
+    /*!
+     * \brief updateRefs Mets à jour les references en parcourrant l'ensemble des notes
+     */
     static void updateRefs();
+
     /*!
      * \brief isNoteReferenced permet de vérifier si une Note
      *  se trouve dans une Référence
      * \param N Note
      */
     static bool isNoteReferenced(Note * N);
+
+    /*!
+     * \brief getReferencesNote retourne les références de la Note en paramètre
+     * \param N La note en question
+     */
     void getReferencesNote(Note * N);
+
+    /*!
+     * \brief getReferences retourne l'ensemble des références de l'ensembled des notes
+     */
     void getReferences();
+
+    /*!
+     * \brief operator * retourne la valeur pointée par l'unique instance
+     */
     Reference& operator* (){return *uniqueRef;}
 };
 
